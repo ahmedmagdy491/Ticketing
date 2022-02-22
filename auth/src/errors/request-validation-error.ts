02e55626c686aded7536f32346815e940bd1045c
unknown <ahmedahmedmagdy491@gmail.com>
2022-02-22 16:49:45 +0200

    first commit

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,16 +1,10 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-interface CustomError {
-	statusCode: number;
-	serializeErrors(): {
-		message: string;
-		field?: string;
-	}[];
-}
-export class RequestValidationError extends Error implements CustomError {
+export class RequestValidationError extends CustomError {
 	statusCode = 400;
 	constructor(public errors: ValidationError[]) {
-		super();
+		super('Ivalid request parameters');
 		Object.setPrototypeOf(this, RequestValidationError.prototype);
 	}
 
