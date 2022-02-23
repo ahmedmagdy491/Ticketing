eac11e01d3da48a1bbee6b74dfa7857ed0767d79
unknown <ahmedahmedmagdy491@gmail.com>
2022-02-23 02:20:29 +0200

    add auth model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signuot';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
-import 'express-async-errors';
 const app = express();
 app.use(json());
 
@@ -20,6 +22,18 @@ app.all('*', async (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(3000, () => {
-	console.log('listening on port 3000');
-});
+
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+		console.log('connected to database');
+	} catch (error) {
+		console.log(error);
+	}
+
+	app.listen(3000, () => {
+		console.log('listening on port 3000');
+	});
+};
+
+start();
